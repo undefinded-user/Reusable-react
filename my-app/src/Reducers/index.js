5ac2a394ca79bf7103571e74dfd6605ae07c4496
Undefined-user <undefinded-user@users.noreply.github.com>
2020-11-21 20:52:52 +0300

    Upgrade  ADD_TO_BASKET feature

--- my-app/src/Reducers/index.js
+++ my-app/src/Reducers/index.js
@@ -1,7 +1,25 @@
 export function globalReducer ( state, action ){
 	switch(action.type){
 		case "ADD_TO_BASKET": {
-			return {...state, basket: [action.payload, ...state.basket]}
+			let { basket } = state;
+			let indexItem;
+
+			const isInBasket = basket.find((item, index)=> {
+				if(item.name === action.payload.name){
+					indexItem = index
+					return true
+				}
+				return false
+			})
+
+			if(isInBasket){
+				let newBasket = [...basket];
+				let amountInBasket = newBasket[indexItem].amountInBasket? newBasket[indexItem].amountInBasket + 1 : 1;
+				console.log(amountInBasket)
+				newBasket[indexItem] = {...newBasket[indexItem], amountInBasket};
+				return {...state, basket: newBasket}
+			}
+			return {...state, basket: [{...action.payload, amountInBasket: 1}, ...state.basket]}
 		}
 		case "REMOVE_FROM_BASKET": {
 			return {...state, basket: state.basket.filter((item)=> item.name !== action.payload.name)}
