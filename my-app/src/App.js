13d7e9ab033211703398cc334b598abc28e75644
Undefined-user <undefinded-user@users.noreply.github.com>
2020-11-21 20:56:55 +0300

    Add components ito the app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,11 @@ import { GlobalContext } from './Context/GlobalProvider';
 import ItemCard from './Components/ItemCard';
 import Header from './Components/Header'
 import Logo from './Components/Logo'
+import NavBar from './Components/Navbar'
+import NavItem from './Components/Navbar/NavItem/'
+import Subheader from './Components/Subheader'
+import ProductPage from './Components/ProductPage'
+
 
 
 function App() {
@@ -13,14 +18,22 @@ function App() {
     <div className="App">
         <Header>
           <Logo />
-          <p>There</p>
+          <NavBar>
+            <NavItem reference='#'>
+              Home
+            </NavItem>
+            <NavItem reference='#'>
+              About
+            </NavItem>
+            <NavItem reference='#'>
+              Contact
+            </NavItem>
+          </NavBar>
         </Header>
-        <ItemCard data={data[0]} />
-        <ItemCard data={data[0]} />
-        <ItemCard data={data[0]} />
-        <ItemCard data={data[0]} />
-        <ItemCard data={data[0]} />
-        
+        <Subheader />
+        <ProductPage>
+          {data.map((item)=> <ItemCard data={item} />)}
+        </ProductPage>        
     </div>
   );
 }
